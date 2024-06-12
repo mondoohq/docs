4cbfcb03c29302caf9f98f5bdfb2377d646b5d51
misterpantz <letha@mondoo.com>
2024-06-12 15:35:40 -0700

    sidebar changes

--- sidebars.js
+++ sidebars.js
@@ -142,12 +142,12 @@ module.exports = {
         },
         {
           type: "doc",
-          id: "cnquery/cnquery/cloud/azure/",
+          id: "cnquery/cnquery/cloud/azure",
           label: "Azure",
         },
         {
           type: "doc",
-          id: "cnquery/cloud/gcp/",
+          id: "cnquery/cloud/gcp",
           label: "Google Cloud",
         },
         {

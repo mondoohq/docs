cab9705f3b463377a92f961127dbbaaebd47a49a
Tim Smith <tsmith84@gmail.com>
2024-07-01 14:46:17 -1000

    Turn back on the sitemap
    We're going to integrate this into our main sitemap to get better
    indexing and this *may* also fix search
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -466,6 +466,12 @@ module.exports = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        sitemap: {
+          changefreq: "daily",
+          lastmod: "date",
+          ignorePatterns: ["**/releases/tags/**"],
+          priority: 0.5,
+        },
       },
     ],
   ],
